b1749fc1a71aef6fff4038ad76b93d2ace1b936d
dandavies23 <dan@dandavies23.com>
2022-05-14 14:38:29 +0000

    add allauth, base home templates, home app and page contents

--- barre_fitness/settings.py
+++ barre_fitness/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    # 'home',
+    'home',
     # 'products',
 ]
 
@@ -62,7 +62,10 @@ ROOT_URLCONF = 'barre_fitness.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
