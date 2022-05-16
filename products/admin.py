a3524efa36d62799c908390d155644d4c461e769
dandavies23 <dan@dandavies23.com>
2022-05-16 10:30:25 +0000

    Customised admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,30 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+# removed 'rating' as not important to Barre Fitness shop
+# will be removed possibly add instock number
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'brand',
+        'category',
+        'variants',
+        'stock',  
+        'price',
+        'image',
+    )
+    ordering = ('brand',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
