59432e05a148d4c800d052433d3917e5510ab20c
dandavies23 <dan@dandavies23.com>
2022-05-17 10:49:30 +0000

    bug fix: MEDIA_URL not rooting -  add context_processors.media template

--- barre_fitness/settings.py
+++ barre_fitness/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
