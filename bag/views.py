449a931cf0ee8fee52f8a3d2d8e979fc74b079fa
dandavies23 <dan@dandavies23.com>
2022-05-17 14:35:37 +0000

    update context processor

--- bag/views.py
+++ bag/views.py
@@ -14,7 +14,7 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
-    
+
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
