917be983fcbd5943f0be305df720eb08c60ac96d
dandavies23 <dan@dandavies23.com>
2022-05-18 20:59:34 +0000

    Initial deployment

--- barre_fitness/settings.py
+++ barre_fitness/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,7 +120,7 @@ WSGI_APPLICATION = 'barre_fitness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+DATABASES = {    
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
