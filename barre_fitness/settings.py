30d53dcf8eb5e718447a3273f2230971691e2d67
dandavies23 <dan@dandavies23.com>
2022-05-18 22:18:28 +0000

    056 deployment part 2

--- barre_fitness/settings.py
+++ barre_fitness/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0m7qvt&-1rmg07s-&#uju@bm4ce9-wcgih3z+gf0^#iu2#%_06
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dandavies23-barre-fitness.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'barre_fitness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {    
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {    
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
