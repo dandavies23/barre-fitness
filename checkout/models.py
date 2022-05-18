9fa2bcfd2ec873b30c97adf80250bb1e14589e74
dandavies23 <dan@dandavies23.com>
2022-05-18 11:33:47 +0000

    Add forms admin, and signals

--- checkout/models.py
+++ checkout/models.py
@@ -28,18 +28,18 @@ class Order(models.Model):
         """
         return uuid.uuid4().hex.upper()
 
-        def update_total(self):
-            """
-            Update grand total each time a line item is added,
-            accounting for delivery costs.
-            """
-            self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
-            if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-                self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
-            else:
-                self.delivery_cost = 0
-            self.grand_total = self.order_total + self.delivery_cost
-            self.save()
+    def update_total(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
+        """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
 
     def save(self, *args, **kwargs):
         """
