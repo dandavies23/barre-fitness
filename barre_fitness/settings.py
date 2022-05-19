e9a97a35eac9511f1589a0210c5527ce8b041f5e
dandavies23 <dan@dandavies23.com>
2022-05-19 09:52:00 +0000

    Add static and media file to s3 settings corrected code

--- barre_fitness/settings.py
+++ barre_fitness/settings.py
@@ -186,9 +186,9 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_STORAGE = 'custom.storages.StaticStorage'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages_MediaStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
