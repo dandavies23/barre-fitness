d19fbbd5cc085e4ddcfa89e4ecf6b275460fd311
dandavies23 <dan@dandavies23.com>
2022-06-17 11:58:13 +0000

    #2 Restart project development sprint 2

--- barre_fitness/settings.py
+++ barre_fitness/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-0m7qvt&-1rmg07s-&#uju@bm4ce9-wcgih3z+gf0^#iu2#%_06')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'True'
 
 ALLOWED_HOSTS = ['barre-fitness.herokuapp.com', 'localhost']
 
