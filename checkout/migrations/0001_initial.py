f0d3afe6596dca2ee02f8a60849c2e6b6b27475d
dandavies23 <dan@dandavies23.com>
2022-06-25 12:58:28 +0000

    Add profile app

--- checkout/migrations/0001_initial.py
+++ checkout/migrations/0001_initial.py
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
                 ('full_name', models.CharField(max_length=50)),
                 ('email', models.EmailField(max_length=254)),
                 ('phone_number', models.CharField(max_length=20)),
-                ('country', models.CharField(max_length=40)),
+                ('country', models.CharField(max_length=100)),
                 ('postcode', models.CharField(blank=True, max_length=20, null=True)),
                 ('town_or_city', models.CharField(max_length=40)),
                 ('street_address1', models.CharField(max_length=80)),
--- checkout/models.py
+++ checkout/models.py
@@ -7,9 +7,13 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from products.models import Product
+from profiles.models import UserProfile
+
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -28,7 +32,7 @@ class Order(models.Model):
 
     def _generate_order_number(self):
         """
-        Generate a random, unique order using UUID
+        Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
 
@@ -47,16 +51,16 @@ class Order(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the lineitem total
-        and update the order total.
+        Override the original save method to set the order number
+        if it hasn't been set already.
         """
-
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.order_number
+
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
@@ -75,3 +79,4 @@ class OrderLineItem(models.Model):
 
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
+        
