fa956db33a9dea84e3a98af70baf8f0319d7694a
dandavies23 <dan@dandavies23.com>
2022-06-30 09:28:11 +0000

    Deployment Commit

--- barre_fitness/settings.py
+++ barre_fitness/settings.py
@@ -17,12 +17,14 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-0m7qvt&-1rmg07s-&#uju@bm4ce9-wcgih3z+gf0^#iu2#%_06')
+
+if os.path.isfile('env.py'):
+    import env
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
